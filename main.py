37ee1d7ed87a5d9850e6fdf72bfbbb27dc5a3196
artkoder <md.nikiforov@gmail.com>
2025-07-03 23:12:40 +0200

    update URL

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from aiohttp import web, ClientSession
 logging.basicConfig(level=logging.INFO)
 
 DB_PATH = os.getenv("DB_PATH", "bot.db")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://telegram-post-scheduler.fly.dev")
-TZ_OFFSET = os.getenv("TZ_OFFSET", "+00:00")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://events-bot-new.fly.dev")
+TZ_OFFSET = os.getenv("TZ_OFFSET", "+02:00")
 SCHED_INTERVAL_SEC = int(os.getenv("SCHED_INTERVAL_SEC", "30"))
 
 CREATE_TABLES = [
